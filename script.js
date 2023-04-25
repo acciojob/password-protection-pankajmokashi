f3b7181c371bacc5b789feaa66fb80337b486b83
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-25 18:46:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let input = document.querySelector("input")
+
+function display(){
+	let n = input.value.length
+	let str = ""
+	for(let i = 0; i < n; i++){
+		str = str + "*"
+	}
+	input.value = str
+}
+input.addEventListener("keyup", display)
